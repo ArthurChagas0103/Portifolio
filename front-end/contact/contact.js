7638faa581671471e6044be552aa0776cfab508d
ArthurChagas0103 <arthurgchagas@gmail.com>
2022-10-12 23:47:51 -0300

    12/10/2022

---
+++ front-end/contact/contact.js
@@ -0,0 +1,41 @@
+function mudarCor() {
+    if (window.scrollY > 60) {
+        document.getElementById('navbar').style.backgroundColor = '#6759c2'
+    }
+    else{
+        document.getElementById('navbar').style.backgroundColor = 'rgba(43, 66, 85, 0)'
+    }
+}
+window.addEventListener('scroll', mudarCor)
+
+var TRange = null;
+
+function findString(str) {
+	if (parseInt(navigator.appVersion) < 4) return;
+	var strFound;
+	if (window.find) {
+		strFound = self.find(str);
+		if (!strFound) {
+			strFound = self.find(str, 0, 1);
+			while (self.find(str, 0, 1)) continue;
+		}
+	}
+	else if (navigator.appName.indexOf("Microsoft") != -1) {
+		if (TRange != null) {
+			TRange.collapse(false);
+			strFound = TRange.findText(str);
+			if (strFound) TRange.select();
+		}
+		if (TRange == null || strFound == 0) {
+			TRange = self.document.body.createTextRange();
+			strFound = TRange.findText(str);
+			if (strFound) TRange.select();
+		}
+	}
+	else if (navigator.appName == "Opera") {
+		alert("Opera não suportado")
+		return;
+	}
+	if (!strFound) alert("Texto '" + str + "' não encontrado!")
+	return;
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function mudarCor() {
-    if (window.scrollY > 420) {
+    if (window.scrollY > 60) {
         document.getElementById('navbar').style.backgroundColor = '#6759c2'
     }
     else{
@@ -7,4 +7,38 @@ function mudarCor() {
     }
 }
 window.addEventListener('scroll', mudarCor)
+
+var TRange = null;
+
+function findString(str) {
+	if (parseInt(navigator.appVersion) < 4) return;
+	var strFound;
+	if (window.find) {
+		strFound = self.find(str);
+		if (!strFound) {
+			strFound = self.find(str, 0, 1);
+			while (self.find(str, 0, 1)) continue;
+		}
+	}
+	else if (navigator.appName.indexOf("Microsoft") != -1) {
+		if (TRange != null) {
+			TRange.collapse(false);
+			strFound = TRange.findText(str);
+			if (strFound) TRange.select();
+		}
+		if (TRange == null || strFound == 0) {
+			TRange = self.document.body.createTextRange();
+			strFound = TRange.findText(str);
+			if (strFound) TRange.select();
+		}
+	}
+	else if (navigator.appName == "Opera") {
+		alert("Opera não suportado")
+		return;
+	}
+	if (!strFound) alert("Texto '" + str + "' não encontrado!")
+	return;
+}
+
+
 
